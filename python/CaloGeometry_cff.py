96fa1040121950eccd9a7fbe465d97766830baf3
cmsbuild <>
2008-04-22 21:26:16 +0000

    This commit was manufactured by cvs2git to create tag 'Geometry- CaloEventSetup-V03-05-02'.
    Sprout from master 2008-04-22 21:26:15 UTC Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch> 'Improve config structure and in test get aligned geometries'
    Cherrypick from master 2008-04-21 22:14:23 UTC Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch> 'First round of alignment changes':
        python/CaloGeometry_cff.py

--- python/CaloGeometry_cff.py
+++ python/CaloGeometry_cff.py
@@ -4,19 +4,19 @@ import FWCore.ParameterSet.Config as cms
 # This cfi should be included to build the ECAL + HCAL geometry model
 #
 EcalBarrelGeometryEP = cms.ESProducer("EcalBarrelGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False) )
+                                      applyAlignment = cms.untracked.bool(false) )
 
 EcalEndcapGeometryEP = cms.ESProducer("EcalEndcapGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False))
+                                      applyAlignment = cms.untracked.bool(false))
 
 EcalPreshowerGeometryEP = cms.ESProducer("EcalPreshowerGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False))
+                                      applyAlignment = cms.untracked.bool(false))
 
 HcalHardcodeGeometryEP = cms.ESProducer("HcalHardcodeGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False))
+                                      applyAlignment = cms.untracked.bool(false))
 
 ZdcHardcodeGeometryEP = cms.ESProducer("ZdcHardcodeGeometryEP",
-                                      applyAlignment = cms.untracked.bool(False))
+                                      applyAlignment = cms.untracked.bool(false))
 
 CaloTowerHardcodeGeometryEP = cms.ESProducer("CaloTowerHardcodeGeometryEP")
 

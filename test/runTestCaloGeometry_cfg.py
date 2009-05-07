88ce020b2821b0cc62d9a7598cd1e49973aa57cb
Brian Keith Heltsley <sha1-bf14517c6b42de43025d4b84714bc1bd4f19dd6b@cern.ch>
2009-05-07 22:07:08 +0000

    Continue to clarify validation scripts

--- test/runTestCaloGeometry_cfg.py
+++ test/runTestCaloGeometry_cfg.py
@@ -7,13 +7,7 @@ process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 
 process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
 
-#process.load("Geometry.CaloEventSetup.calodbsqlitefile")
-
 process.load("Geometry.CaloEventSetup.CaloGeometry_cff")
-
-#process.load("Geometry.CaloEventSetup.AlignedCaloGeometry_cfi")
-
-#process.load("Geometry.CaloEventSetup.FakeCaloAlignments_cff")
 
 process.load("Geometry.CaloEventSetup.CaloTopology_cfi")
 
@@ -21,7 +15,7 @@ process.load("Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(4)  )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(4) )
 
 process.source = cms.Source("EmptySource")
 
@@ -30,7 +24,8 @@ process.etta = cms.EDFilter("dumpEcalTrigTowerMapping")
 process.ctgw = cms.EDFilter("testEcalGetWindow")
 
 process.cga = cms.EDAnalyzer("CaloGeometryAnalyzer",
-                             fullEcalDump = cms.untracked.bool(True) )
+                             fullEcalDump = cms.untracked.bool(True)
+                             )
 
 process.mfa = cms.EDFilter("testMagneticField")
 
@@ -38,7 +33,9 @@ process.Timing = cms.Service("Timing")
 
 process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck")
 
-process.TFileService = cms.Service("TFileService", fileName = cms.string('calogeom.root') )
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('calogeom.root')
+                                   )
 
 process.p1 = cms.Path(process.etta*process.ctgw*process.cga*process.mfa)
 
